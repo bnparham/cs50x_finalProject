0c85b11c615898c21ef8a941d565ae0447fceaab
bnparham <parhambn.workmail@gmail.com>
2023-08-23 00:32:50 +0430

    update database name

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///databse.db")
+db = SQL("sqlite:///database.db")
 
 OFFSET = {'offset' : 0}
 POST_ID = {}
